2977773752943e3451a253e91fd66bf15cd09e33
Sourav Chakraborty <sourav@Souravs-MacBook-Pro.local>
2025-02-19 00:30:57 +0530

    project setup react with vite and tailwind css

---
+++ portfolio-app/src/App.jsx
@@ -0,0 +1,27 @@
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <h1 className="text-3xl font-bold underline ">
+          Hello world!
+      </h1>
+      <p className="">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
